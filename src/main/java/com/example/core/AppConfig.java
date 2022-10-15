173e1a704cdb6e169309bd3cbb07a61207ac9827
kim0914 <kosa0914@naver.com>
2022-10-15 23:24:40 +0900

    Review: add configuration appconfig test

--- src/main/java/com/example/core/AppConfig.java
+++ src/main/java/com/example/core/AppConfig.java
@@ -7,26 +7,33 @@ import com.example.core.member.MemberService;
 import com.example.core.member.MemberServiceImpl;
 import com.example.core.member.MemoryMemberRepository;
 import com.example.core.order.OrderService;
+import com.example.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService(){
+        System.out.println("Call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository(){
+        System.out.println("Call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
-//    @Bean
-//    public OrderService orderService(){
-//        return new OrdreServiceImpl(memberRepository(), disCountPolicy());
-//    }
+    @Bean
+    public OrderService orderService(){
+        System.out.println("Call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), disCountPolicy());
+    }
 
     @Bean
     public DiscountPolicy disCountPolicy(){
--- src/main/java/com/example/core/member/MemberServiceImpl.java
+++ src/main/java/com/example/core/member/MemberServiceImpl.java
@@ -18,6 +18,11 @@ public class MemberServiceImpl implements MemberService{
 
     @Override
     public Member findMember(Long memberId) {
-        return null;
+        return memberRepository.findById(memberId);
+    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
