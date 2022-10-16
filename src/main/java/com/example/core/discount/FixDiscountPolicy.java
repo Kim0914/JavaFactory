43b143d663da986cc93036167e463ea3a9a4856e
kim0914 <kosa0914@naver.com>
2022-10-16 22:21:33 +0900

    Review: modify configuration to component annotation

--- src/main/java/com/example/core/discount/FixDiscountPolicy.java
+++ src/main/java/com/example/core/discount/FixDiscountPolicy.java
@@ -4,7 +4,7 @@ import com.example.core.member.Grade;
 import com.example.core.member.Member;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000; // 1000원 할인
 
--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -22,8 +22,8 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         // 주문이 들어오면, 먼저 회원정보를 검색하고 할인정책을 거친 후 주문 생성
-        Member member = memberRepository.findById(memberId); // OrderService의 입장에서는, 야 db! 너는 니 알아서 멤버 찾아온나 나는 주문만 관여한다는 마인드
-        int discountPrice = discountPolicy.discount(member, itemPrice); // OrderService의 입장에서는, 야 할인! 너는 니 알아서 해라 나는 주문만 관여한다는 마인드
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
         // 이렇게 되면 할인을 고치고 싶으면 할인만 건들면 된다.
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
