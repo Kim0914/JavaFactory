1c70ead645b151199207f4f0db204388d68eef4c
kim0914 <kosa0914@naver.com>
2022-10-18 21:32:59 +0900

    Review: various dependency injection ways

--- src/main/java/com/example/core/order/OrderServiceImpl.java
+++ src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -12,13 +12,27 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    /** 필드 주입 -> 권장 X
+     * @Autowired private final MemberRepository memberRepository;
+     * @Autowired private final DiscountPolicy discountPolicy
+     */
 
     @Autowired // 자동으로 의존 관계 대입 (생성자 주입)
-    public OrderServiceImpl(MemberRepository memberRepository,  DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
+    /** 수정자 주입
+     * @Autowired
+     public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    }
+    **/
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         // 주문이 들어오면, 먼저 회원정보를 검색하고 할인정책을 거친 후 주문 생성
